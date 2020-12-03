9f794c9ac623b5a7bccba2bfb684cb046ad4e107
Quentin Bernet <quentin.bernet@bluewin.ch>
2020-12-03 17:10:09 +0100

    Add constructors for Vector of 0, 1 and 2 elements

--- src/main/scala/Vector.scala
+++ src/main/scala/Vector.scala
@@ -1,13 +1,19 @@
 import scala.compiletime.S
-import scala.compiletime.ops.int._
 import scala.quoted._
 
 
 trait Vector[T, A <: Arity]{
   def apply[At <: Arity](index: At): T
 }
-case object VNil                                          extends Vector[Any, 0]{
-  def apply[At <: Arity](index: At): Unit = ()
+
+object Vector{
+  def apply[T](): Vector[T, 0] = VNil[T]()
+  def apply[T](e0: T): Vector[T, 1] = Cons(e0, Vector())
+  def apply[T](e0: T, e1: T): Vector[T, 2] = Cons(e0, Vector(e1))
+}
+
+case class VNil[T]()                                         extends Vector[T, 0]{
+  def apply[At <: Arity](index: At): T = throw new IllegalArgumentException(f"Acces on index ${index} of an empty Vector (This means your index was ${index+1} bigger than the size of your Vector)")
 }
 case class Cons[T, A <: Arity](head: T, tail: Vector[T,A]) extends Vector[T, S[A]]{
 
@@ -17,7 +23,8 @@ case class Cons[T, A <: Arity](head: T, tail: Vector[T,A]) extends Vector[T, S[A
       head
     }else{
       //val nIndex: At-1 = index - 1
-      val nIndex = dirtyTrick(index-1)
+      val nIndex: Arity = index - 1
+      //val nIndex = dirtyTrick(index-1)
       tail.apply(nIndex)
     }
   }
