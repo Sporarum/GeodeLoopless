8c8757051a99cf03e24421f0720bca2a5887dbeb
Quentin Bernet <quentin.bernet@bluewin.ch>
2020-12-03 18:38:11 +0100

    Add Comp class: represents function composition

--- src/main/scala/primrec.scala
+++ src/main/scala/primrec.scala
@@ -17,6 +17,10 @@ case class Succ() extends PrimRecFun[1] {
   def apply(args: V) = SuccNat(args(0))
 }
 
+case class Comp[A1 <: Arity, A2 <: Arity](g: PrimRecFun[A1], fs: Vector[PrimRecFun[A2], A1]) extends PrimRecFun[A2]{
+  def apply(args: V) = g(fs.map(_(args)))
+}
+
 case class PrimRecSet[A <: Arity](fun: PrimRecFun[A]) {
   def contains(elem: Vector[Nat, A]): Boolean = 
     fun(elem) match {
