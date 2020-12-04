50a54f08fbf07c9fa41ab4661f57ac0aa9d7460d
Quentin Bernet <quentin.bernet@bluewin.ch>
2020-12-04 23:26:05 +0100

    Add methods `head`, `tail`, and `*:` to Vector

--- src/main/scala/Vector.scala
+++ src/main/scala/Vector.scala
@@ -1,10 +1,20 @@
 import scala.compiletime.S
+import scala.compiletime.error
+import annotation.showAsInfix
+import scala.annotation.implicitNotFound
 import scala.quoted._
 
-
 trait Vector[T, A <: Arity]{
+  /*@implicitNotFound("index out of bounds")
+  type Check[At <: Arity] <: T = A <= At match {
+    case true => T
+    //case _ => error("")
+  }*/
   def apply[At <: Arity](index: At): T
   def map[U](f: T => U): Vector[U, A]
+  def head(): T
+  def tail(): Vector[T, P[A]]
+  inline def *: (x: T): Vector[T, S[A]] = Cons(x, this)
 }
 
 object Vector{
@@ -13,27 +23,24 @@ object Vector{
   def apply[T](e0: T, e1: T): Vector[T, 2] = Cons(e0, Vector(e1))
 }
 
-case class VNil[T]()                                       extends Vector[T, 0]{
+case class VNil[T]() extends Vector[T, 0]{
   def apply[At <: Arity](index: At): T = throw new IllegalArgumentException(f"Acces on index ${index} of an empty Vector (This means your index was ${index+1} bigger than the size of your Vector)")
   def map[U](f: T => U) = VNil[U]()
+  def head() = throw new IllegalArgumentException(f"Empty Vector has no head")
+  def tail() = throw new IllegalArgumentException(f"Empty Vector has no tail")
 }
-case class Cons[T, A <: Arity](head: T, tail: Vector[T,A]) extends Vector[T, S[A]]{
 
+case class Cons[T, A <: Arity](h: T, t: Vector[T,A]) extends Vector[T, S[A]]{
   def apply[At <: Arity](index: At): T = {
-    //val arityCheck: At <= A = true
     return if(index == 0){
-      head
+      h
     }else{
-      //val nIndex: At-1 = index - 1
-      val nIndex: Arity = index - 1
-      //val nIndex = dirtyTrick(index-1)
-      tail.apply(nIndex)
+      val nIndex: P[At] = minusOne(index);
+      t.apply(nIndex)
     }
   }
 
-  private def dirtyTrick(i: Int): Arity = {
-    return i
-  }
-
-  def map[U](f: T => U) = Cons(f(head), tail.map(f))
+  def map[U](f: T => U) = Cons(f(h), t.map(f))
+  def head() = h
+  def tail() = t
 }
