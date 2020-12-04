f3f44652bc9c65902f3b6bd936108cb309f20726
Quentin Bernet <quentin.bernet@bluewin.ch>
2020-12-04 23:30:09 +0100

    Add PrimRecFun `Rec` for recursion

--- src/main/scala/primrec.scala
+++ src/main/scala/primrec.scala
@@ -1,3 +1,4 @@
+import scala.compiletime.S
 
 trait PrimRecFun[A <: Arity] {
   type V = Vector[Nat, A]
@@ -22,6 +23,18 @@ case object Succ extends PrimRecFun[1] {
 //f(X) = g(f_1(X), f_2(X), ... , f_A1(X)) with X a vector of arity A2
 case class Comp[A1 <: Arity, A2 <: Arity](g: PrimRecFun[A1], fs: Vector[PrimRecFun[A2], A1]) extends PrimRecFun[A2]{
   def apply(args: V) = g(fs.map(_(args)))
+}
+
+// f(0 *: X) = init(X) with X a vector of arity A1
+// f(S(n) *: X) = step(f(n *: X) *: n *: X)
+case class Rec[A1 <: Arity](init: PrimRecFun[A1], step: PrimRecFun[S[S[A1]]]) extends PrimRecFun[S[A1]]{
+  def apply(args: V) = 
+    val head = args.head()
+    val tail: Vector[Nat, A1] = args.tail()
+    head match
+      case ZeroNat => init(tail)
+      case SuccNat(n) =>
+        step(this.apply(n *: tail) *: n *: tail )
 }
 
 case class PrimRecSet[A <: Arity](fun: PrimRecFun[A]) {
