071f7e7259efbcd5e9cd859d9ba847e2de5b5452
Quentin Bernet <quentin.bernet@bluewin.ch>
2020-12-04 23:30:32 +0100

    Add more examples to Main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,8 +2,12 @@
 object Main {
 
   def main(args: Array[String]): Unit = {
-    val proj: PrimRecFun[2] = Proj(3)
-    val res = proj(Vector(Nat(1), Nat(4)))
+    val proj = Proj[0](0) //Should fail
+    val succ: PrimRecFun[1] = Succ
+    val t: P[0] = minusOne(0) //Should fail, but at least is not -1
+    val vec = Nat(4) *: Nat(3) *: VNil()
+    val res = proj(Vector())
+    //val res2 = proj(Nat(1) ** (Nat(4) -: VNil()))
     println(res)
   }
 
