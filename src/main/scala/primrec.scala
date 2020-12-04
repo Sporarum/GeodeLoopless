c218ff89e2a5b359d6ce90a2ba2f8c0887f81184
Quentin Bernet <quentin.bernet@bluewin.ch>
2020-12-04 23:19:36 +0100

    Add comments before PrimRecFuns to add clarity

--- src/main/scala/primrec.scala
+++ src/main/scala/primrec.scala
@@ -4,19 +4,22 @@ trait PrimRecFun[A <: Arity] {
   def apply(args: Vector[Nat, A]): Nat
 }
 
-
+//f(X) = c
 case class Const[A <: Arity](val constant: Nat) extends PrimRecFun[A] {
   def apply(args: V) = constant
 }
 
+//f(X) = x_n
 case class Proj[A <: Arity](val n: Arity) extends PrimRecFun[A] {
   def apply(args: V) = args(n)
 }
 
-case class Succ() extends PrimRecFun[1] {
+//f(x) = Succ(x)
+case object Succ extends PrimRecFun[1] {
   def apply(args: V) = SuccNat(args(0))
 }
 
+//f(X) = g(f_1(X), f_2(X), ... , f_A1(X)) with X a vector of arity A2
 case class Comp[A1 <: Arity, A2 <: Arity](g: PrimRecFun[A1], fs: Vector[PrimRecFun[A2], A1]) extends PrimRecFun[A2]{
   def apply(args: V) = g(fs.map(_(args)))
 }
@@ -24,7 +27,7 @@ case class Comp[A1 <: Arity, A2 <: Arity](g: PrimRecFun[A1], fs: Vector[PrimRecF
 case class PrimRecSet[A <: Arity](fun: PrimRecFun[A]) {
   def contains(elem: Vector[Nat, A]): Boolean = 
     fun(elem) match {
-      case ZeroNat => false
+      case ZeroNat => false 
       case _ => true
     }
 }
