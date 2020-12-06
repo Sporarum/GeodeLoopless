7f79947f1f2c962e1b1a1451dcdd983a13fefd32
Quentin Bernet <quentin.bernet@bluewin.ch>
2020-12-06 01:16:25 +0100

    Add mkString, toList and unzip to Vector

--- src/main/scala/Vector.scala
+++ src/main/scala/Vector.scala
@@ -14,6 +14,10 @@ trait Vector[T, A <: Arity]{
   def map[U](f: T => U): Vector[U, A]
   def head(): T
   def tail(): Vector[T, P[A]]
+  def mkString(start: String = "", sep: String = "", end: String = ""): String
+  def mkString_(sep: String, end: String): String //TODO: add protected or something
+  def toList(): List[T]
+  def unzip[T1, T2](implicit asPair: T => (T1, T2)): (Vector[T1, A], Vector[T2, A])
   inline def *: (x: T): Vector[T, S[A]] = Cons(x, this)
 }
 
@@ -28,19 +32,31 @@ case class VNil[T]() extends Vector[T, 0]{
   def map[U](f: T => U) = VNil[U]()
   def head() = throw new IllegalArgumentException(f"Empty Vector has no head")
   def tail() = throw new IllegalArgumentException(f"Empty Vector has no tail")
+  def mkString(start: String = "", sep: String = "", end: String = ""): String = start ++ end
+  def mkString_(sep: String, end: String): String = end
+  def toList() = Nil
+  def unzip[T1, T2](implicit asPair: T => (T1, T2)) = (VNil[T1](), VNil[T2]())
 }
 
 case class Cons[T, A <: Arity](h: T, t: Vector[T,A]) extends Vector[T, S[A]]{
-  def apply[At <: Arity](index: At): T = {
-    return if(index == 0){
+  def apply[At <: Arity](index: At): T = 
+    if(index == 0){
       h
     }else{
       val nIndex: P[At] = minusOne(index);
       t.apply(nIndex)
     }
-  }
 
   def map[U](f: T => U) = Cons(f(h), t.map(f))
   def head() = h
   def tail() = t
+  def mkString(start: String = "", sep: String = "", end: String = ""): String = 
+    start ++ h.toString ++ t.mkString(sep, end)
+  def mkString_(sep: String, end: String): String = 
+    sep ++ h.toString ++ t.mkString_(sep,end)
+  def toList() = h :: t.toList()
+  def unzip[T1, T2](implicit asPair: T => (T1, T2)) = 
+    val (h1, h2) = asPair(h)
+    val (t1, t2) = t.unzip(asPair)
+    (h1 *: t1, h2 *: t2)
 }
