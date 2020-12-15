5261c2a3c12ee743c2a57cced511ff50db8f380d
Quentin Bernet <quentin.bernet@bluewin.ch>
2020-12-15 14:05:14 +0100

    Change `Rec`, now does recursion on last element

--- src/main/scala/primrec.scala
+++ src/main/scala/primrec.scala
@@ -3,7 +3,12 @@ import scala.compiletime.S
 trait PrimRecFun[A <: Arity] {
   type V = Vector[Nat, A]
   def apply(args: V): Nat
-  def debug(args: V): (Nat, String)
+  def debug(args: V): (Nat, String) = this match{
+    case UserDefined(_, f) => f.debug_(args) //If we are debugging A user defined, debug the inside
+    case _ => this.debug_(args)
+  }
+  def debug_(args: V): (Nat, String)
+
   def prettyDebug(args: V): String =
     val string = debug(args)._2
     Util.indentFromBraces(string.split("\n").toList).mkString("\n")
@@ -11,8 +16,8 @@ trait PrimRecFun[A <: Arity] {
 
 case class UserDefined[A <: Arity](name: String, f: PrimRecFun[A]) extends PrimRecFun[A] {
   def apply(args: V) = f(args)
-  def debug(args: V) = 
-    val res = f(args)
+  def debug_(args: V) = 
+    val res = f(args) //hides the encased complexity for debugging
     val n = "\n"
     (res, f"Function $name on args: $args${n}Returned: $res")
 }
@@ -20,64 +25,64 @@ case class UserDefined[A <: Arity](name: String, f: PrimRecFun[A]) extends PrimR
 //f(X) = constant
 case class Const[A <: Arity](val constant: Nat) extends PrimRecFun[A] {
   def apply(args: V) = constant
-  def debug(args: V) =
+  def debug_(args: V) =
     (apply(args), f"Const($constant) on args: $args") //print arity ?
 }
 
 //f(X) = x_n
 case class Proj[A <: Arity](val n: Arity) extends PrimRecFun[A] {
   def apply(args: V) = args(n)
-  def debug(args: V) =
+  def debug_(args: V) =
     (apply(args), f"Proj($n) on args: $args")
 }
 
 //f(x) = Succ(x)
 case object Succ extends PrimRecFun[1] {
   def apply(args: V) = SuccNat(args(0))
-  def debug(args: V) =
+  def debug_(args: V) =
     (apply(args), f"Succ on args: $args") 
 }
 
 //f(X) = g(f_1(X), f_2(X), ... , f_A1(X)) with X a vector of arity A2
 case class Comp[A1 <: Arity, A2 <: Arity](g: PrimRecFun[A1], fs: Vector[PrimRecFun[A2], A1]) extends PrimRecFun[A2]{
   def apply(args: V) = g(fs.map(_(args)))
-  def debug(args: V) =
+  def debug_(args: V) =
     val n = '\n'
     val start = f"Comp on args: $args {$n"
-    val (resultsFs, strings) = fs.map(_.debug(args)).unzip
+    val (resultsFs, strings) = fs.map(_.debug_(args)).unzip
     val stringsWithNames = strings.toList().zipWithIndex.map{case (s, m) => f"f_$m: {$n$s$n}"} //add result after } ?
     val stringFs: String = stringsWithNames.mkString(start="", sep="\n", end=f"$n")
-    val (resultG, stringG) = g.debug(resultsFs)
+    val (resultG, stringG) = g.debug_(resultsFs)
     (resultG, start ++ stringFs ++ f"g: {$n$stringG$n}: $resultG" ++ f"$n}: $resultG")
 
 }
 
-// f(0 *: X) = init(X) with X a vector of arity A1
-// f(S(n) *: X) = step(f(n *: X) *: n *: X)
-case class Rec[A1 <: Arity](init: PrimRecFun[A1], step: PrimRecFun[S[S[A1]]]) extends PrimRecFun[S[A1]]{
+// f(X :* 0) = init(X) with X a vector of arity A1
+// f(X :* S(n)) = step(X :* n :* f(X :* n))
+case class Rec[A1 <: Arity](base: PrimRecFun[A1], step: PrimRecFun[S[S[A1]]]) extends PrimRecFun[S[A1]]{
   def apply(args: V) = 
-    val head = args.head()
-    val tail: Vector[Nat, A1] = args.tail()
-    head match
-      case ZeroNat => init(tail)
+    val last = args.last()
+    val init: Vector[Nat, A1] = args.init()
+    last match
+      case ZeroNat => base(init)
       case SuccNat(n) =>
-        step(this.apply(n *: tail) *: n *: tail )
+        step(init :* n :* apply(init :* n))
 
-  def debug(args: V) =
+  def debug_(args: V) =
     val n = '\n'
-    def debug_(args: V): (Nat, String) =
-      val head = args.head()
-      val tail: Vector[Nat, A1] = args.tail()
-      head match
+    def debug_inner(args: V): (Nat, String) =
+      val last = args.last()
+      val init: Vector[Nat, A1] = args.init()
+      last match
         case ZeroNat => 
-          val (res, s) = init.debug(tail)
+          val (res, s) = base.debug_(init)
           (res, f"Rec calls init: {$n$s$n}: $res")
         case SuccNat(pred) =>
-          val (recRes, recS) = debug_(pred *: tail)
-          val (stepRes, stepS) = step.debug(recRes *: pred *: tail)
+          val (recRes, recS) = debug_inner(init :* pred)
+          val (stepRes, stepS) = step.debug_(init :* pred :* recRes)
           (stepRes, recS ++ "\n" ++ f"Rec calls step: {$n$stepS$n}: $stepRes")
 
-    val (res, s) = debug_(args)
+    val (res, s) = debug_inner(args)
     (res, f"Rec on args: $args {$n$s$n}: $res")
     
 }
