335956a8470d52f701f56220453d7ae30c8898c5
Quentin Bernet <quentin.bernet@bluewin.ch>
2021-01-24 00:24:57 +0100

    Refactor Cons to +: for brevity

--- src/main/scala/Vector.scala
+++ src/main/scala/Vector.scala
@@ -14,10 +14,13 @@ trait Vector[T, A <: Arity]{
   //type Geq[At] = A <= At
   def apply[At <: Arity](index: At): T
   def map[U](f: T => U): Vector[U, A]
+
+  //TODO: All of those should be able to throw type errors if VNil
   def head(): T
   def tail(): Vector[T, P[A]]
   def init(): Vector[T, P[A]]
   def last(): T
+
   def mkString(start: String = "", sep: String = "", end: String = ""): String
   def mkString_(sep: String, end: String): String //TODO: add protected or something
   def toList(): List[T]
@@ -25,15 +28,15 @@ trait Vector[T, A <: Arity]{
 
   override def toString(): String = this.mkString("Vector(", ", ", ")")
 
-  inline def *: (x: T): Vector[T, S[A]] = Cons(x, this)
-  def :* (x: T): Vector[T, S[A]] = appended(x)
+  inline def +: (x: T): Vector[T, S[A]] = new +:(x, this)
+  def :+ (x: T): Vector[T, S[A]] = appended(x)
   def appended(x: T): Vector[T, S[A]]
 }
 
 object Vector{
   def apply[T](): Vector[T, 0] = VNil[T]()
-  def apply[T](e0: T): Vector[T, 1] = Cons(e0, Vector())
-  def apply[T](e0: T, e1: T): Vector[T, 2] = Cons(e0, Vector(e1))
+  def apply[T](e0: T): Vector[T, 1] = +:(e0, Vector())
+  def apply[T](e0: T, e1: T): Vector[T, 2] = +:(e0, Vector(e1))
 }
 
 case class VNil[T]() extends Vector[T, 0]{
@@ -47,26 +50,26 @@ case class VNil[T]() extends Vector[T, 0]{
   def mkString_(sep: String, end: String): String = end
   def toList() = Nil
   def unzip[T1, T2](implicit asPair: T => (T1, T2)): (Vector[T1, 0], Vector[T2, 0]) = (VNil[T1](), VNil[T2]())
-  def appended(x: T): Vector[T, 1] = Cons(x, VNil())
+  def appended(x: T): Vector[T, 1] = x +: VNil()
 }
 
-case class Cons[T, A <: Arity](h: T, t: Vector[T,A]) extends Vector[T, S[A]]{
+case class +:[T, A <: Arity](h: T, t: Vector[T,A]) extends Vector[T, S[A]]{
   def apply[At <: Arity](index: At): T = 
     if(index == 0){
       h
     }else{
-      val nIndex: P[At] = minusOne(index);
+      val nIndex: P[At] = minusOne(index)
       t.apply(nIndex)
     }
 
-  def map[U](f: T => U) = Cons(f(h), t.map(f))
+  def map[U](f: T => U) = f(h) +: t.map(f)
   def head() = h
   def tail() = t
   def init() =
     def fix[T, A <: Arity](v: Vector[T, S[P[A]]]): Vector[T, A] =  v.asInstanceOf[Vector[T, A]]
     t match
       case VNil() => t
-      case _ => val res = Cons(h, t.init()); fix(res)
+      case _ => val res = h +: t.init(); fix(res)
   def last() = 
     t match
       case VNil() => h
@@ -80,8 +83,8 @@ case class Cons[T, A <: Arity](h: T, t: Vector[T,A]) extends Vector[T, S[A]]{
   def unzip[T1, T2](implicit asPair: T => (T1, T2)) = 
     val (h1, h2) = asPair(h)
     val (t1, t2) = t.unzip(asPair)
-    (h1 *: t1, h2 *: t2)
+    (h1 +: t1, h2 +: t2)
 
   
-  def appended(x: T): Vector[T, S[S[A]]] = h *: t.appended(x)
+  def appended(x: T): Vector[T, S[S[A]]] = h +: t.appended(x)
 }
