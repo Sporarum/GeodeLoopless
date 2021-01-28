8f36cdd50493ea9f630a7ace8459686c09170573
Quentin Bernet <quentin.bernet@bluewin.ch>
2021-01-29 00:45:45 +0100

    Implement exp, fixed base exponentiation

--- src/main/scala/DerivedFunctions.scala
+++ src/main/scala/DerivedFunctions.scala
@@ -9,5 +9,8 @@ def add: PrimRecFun[2] = UserDefined("add", Rec(Proj(0), Succ on Proj(2)))
 
 def mult: PrimRecFun[2] = UserDefined("mult", Rec(Const(Nat(0)), add(Proj(2), Proj(0))))
 
+def exp(base: Nat): PrimRecFun[1] = UserDefined("exp", Rec(Const(Nat(0)), mult(Proj(1), Const(base))))
+
+
 //f(z *: X) = 0 if for all t <= z: (t *: X) not in A
 def boundedMin[A <: Arity](set: PrimRecSet[A]): PrimRecFun[A] = ???
