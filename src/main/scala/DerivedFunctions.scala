f7f3bea6ef032460ee7965d05a6ec5175da59484
Quentin Bernet <quentin.bernet@bluewin.ch>
2021-01-28 21:49:42 +0100

    Implement add

--- src/main/scala/DerivedFunctions.scala
+++ src/main/scala/DerivedFunctions.scala
@@ -3,7 +3,9 @@ def identity[A <: Arity](n: A): PrimRecFun[A] = ???
 
 def subDotOne: PrimRecFun[1] = UserDefined("subDotOne", Rec(Const(Nat(0)),Proj(0)))
 
-def subDot: PrimRecFun[2] = Rec(Proj(0), Comp(subDotOne, Vector(Proj(2))))
+def subDot: PrimRecFun[2] = UserDefined("subDot", Rec(Proj(0), Comp(subDotOne, Vector(Proj(2)))))
+
+def add: PrimRecFun[2] = UserDefined("add", Rec(Proj(0), Succ on Proj(2)))
 
 //f(z *: X) = 0 if for all t <= z: (t *: X) not in A
 def boundedMin[A <: Arity](set: PrimRecSet[A]): PrimRecFun[A] = ???
