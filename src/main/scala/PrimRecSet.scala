710bdd52cf54b12ddcbbf0beb9cbb901b130ec67
Quentin Bernet <quentin.bernet@bluewin.ch>
2021-01-29 22:36:51 +0100

    Restrict `PrimRecSet` to `chi` with image {0,1}

--- src/main/scala/PrimRecSet.scala
+++ src/main/scala/PrimRecSet.scala
@@ -5,6 +5,7 @@ sealed case class PrimRecSet[A <: Arity](chi: PrimRecFun[A]) {
   def contains(elem: Vector[Nat, A]): Boolean = 
     chi(elem) match {
       case ZeroNat => false 
-      case _ => true
+      case SuccNat(ZeroNat) => true
+      case _ => throw IllegalStateException("This set's characteristic function returned something different from 0 or 1")
     }
 }
