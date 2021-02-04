d6c056acbc997f6d6720949c32d0763b9ad8217d
Quentin Bernet <quentin.bernet@bluewin.ch>
2021-02-04 17:37:56 +0100

    Refactor to new indent syntax

--- src/main/scala/PrimRecFun.scala
+++ src/main/scala/PrimRecFun.scala
@@ -1,50 +1,51 @@
 import scala.compiletime.S
 
-sealed trait PrimRecFun[A <: Arity] {
+sealed trait PrimRecFun[A <: Arity]:
   type V = Vector[Nat, A]
   def apply(args: V): Nat
-  def debug(args: V): (Nat, String) = this match{
-    case UserDefined(_, f) => f.debug_(args) //If we are debugging a user defined function, debug the inside
-    case _ => this.debug_(args)
-  }
+  def debug(args: V): (Nat, String) = 
+    this match
+      case UserDefined(_, f) => f.debug_(args) //If we are debugging a user defined function, debug the inside
+      case _ => this.debug_(args)
+    
   def debug_(args: V): (Nat, String)
 
   def prettyDebug(args: V): String =
     val string = debug(args)._2
     Util.indentFromBraces(string.split("\n").toList).mkString("\n")
-}
 
-sealed case class UserDefined[A <: Arity](name: String, f: PrimRecFun[A]) extends PrimRecFun[A] {
+
+sealed case class UserDefined[A <: Arity](name: String, f: PrimRecFun[A]) extends PrimRecFun[A]:
   def apply(args: V) = f(args)
   def debug_(args: V) = 
     val res = apply(args) //hides the encased complexity for debugging
     val n = "\n"
     (res, f"Function $name on args: $args${n}Returned: $res")
-}
+
 
 //f(X) = constant
-sealed case class Const[A <: Arity](val constant: Nat) extends PrimRecFun[A] {
+sealed case class Const[A <: Arity](val constant: Nat) extends PrimRecFun[A]:
   def apply(args: V) = constant
   def debug_(args: V) =
     (apply(args), f"Const($constant) on args: $args") //print arity ?
-}
+
 
 //f(X) = x_n
-sealed case class Proj[A <: Arity](val n: Arity) extends PrimRecFun[A] {
+sealed case class Proj[A <: Arity](val n: Arity) extends PrimRecFun[A]:
   def apply(args: V) = args(n)
   def debug_(args: V) =
     (apply(args), f"Proj($n) on args: $args")
-}
+
 
 //f(x) = Succ(x)
-case object Succ extends PrimRecFun[1] {
+case object Succ extends PrimRecFun[1]:
   def apply(args: V) = SuccNat(args(0))
   def debug_(args: V) =
     (apply(args), f"Succ on args: $args") 
-}
+
 
 //f(X) = g(f_1(X), f_2(X), ... , f_A1(X)) with X a vector of arity A2
-sealed case class Comp[A1 <: Arity, A2 <: Arity](g: PrimRecFun[A1], fs: Vector[PrimRecFun[A2], A1]) extends PrimRecFun[A2]{
+sealed case class Comp[A1 <: Arity, A2 <: Arity](g: PrimRecFun[A1], fs: Vector[PrimRecFun[A2], A1]) extends PrimRecFun[A2]:
   def apply(args: V) = g(fs.map(_(args)))
   def debug_(args: V) =
     val n = '\n'
@@ -55,11 +56,10 @@ sealed case class Comp[A1 <: Arity, A2 <: Arity](g: PrimRecFun[A1], fs: Vector[P
     val (resultG, stringG) = g.debug_(resultsFs)
     (resultG, start ++ stringFs ++ f"g: {$n$stringG$n}: $resultG" ++ f"$n}: $resultG")
 
-}
 
 // f(X :+ 0) = init(X) with X a vector of arity A1
 // f(X :+ S(n)) = step(X :+ n :+ f(X :+ n))
-sealed case class Rec[A1 <: Arity](base: PrimRecFun[A1], step: PrimRecFun[S[S[A1]]]) extends PrimRecFun[S[A1]]{
+sealed case class Rec[A1 <: Arity](base: PrimRecFun[A1], step: PrimRecFun[S[S[A1]]]) extends PrimRecFun[S[A1]]:
   def apply(args: V) = 
     val last = args.last()
     val init: Vector[Nat, A1] = args.init()
@@ -84,8 +84,7 @@ sealed case class Rec[A1 <: Arity](base: PrimRecFun[A1], step: PrimRecFun[S[S[A1
 
     val (res, s) = debug_inner(args)
     (res, f"Rec on args: $args {$n$s$n}: $res")
-    
-}
+
 
 extension [A0 <: Arity] (g: PrimRecFun[1])
   def apply(f0: PrimRecFun[A0]) =
--- src/main/scala/PrimRecSet.scala
+++ src/main/scala/PrimRecSet.scala
@@ -1,11 +1,10 @@
 
 
 
-sealed case class PrimRecSet[A <: Arity](chi: PrimRecFun[A]) {
+sealed case class PrimRecSet[A <: Arity](chi: PrimRecFun[A]):
   def contains(elem: Vector[Nat, A]): Boolean = 
-    chi(elem) match {
+    chi(elem) match
       case ZeroNat => false 
       case SuccNat(ZeroNat) => true
       case _ => throw IllegalStateException("This set's characteristic function returned something different from 0 or 1")
-    }
-}
+    
