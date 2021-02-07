2b23775790f1c457e6e3b7ec835add315c0d9530
Quentin Bernet <quentin.bernet@bluewin.ch>
2021-02-07 17:13:55 +0100

    Add `zip{|WithIndex}` to `Vector`

--- src/main/scala/Vector.scala
+++ src/main/scala/Vector.scala
@@ -16,10 +16,10 @@ trait Vector[+T, A <: Arity]:
   def map[U](f: T => U): Vector[U, A]
 
   //TODO: All of those should be able to throw type errors if VNil
-  def head(): T
-  def tail(): Vector[T, P[A]]
-  def init(): Vector[T, P[A]]
-  def last(): T
+  def head: T
+  def tail: Vector[T, P[A]]
+  def init: Vector[T, P[A]]
+  def last: T
 
   def reverse: Vector[T, A]
   def fold[U >: T](z: U)(op: (U,U) => U): U
@@ -30,6 +30,9 @@ trait Vector[+T, A <: Arity]:
   def toList(): List[T]
   def unzip[T1, T2](implicit asPair: T => (T1, T2)): (Vector[T1, A], Vector[T2, A])
 
+  def zip[U](other: Vector[U, A]): Vector[(T,U), A]
+  def zipWithIndex: Vector[(T,Int), A] = zipWithIndex_(0)
+  def zipWithIndex_(offset: Int): Vector[(T,Int), A] //TODO: add protected, seems to cause problems
   override def toString(): String = this.mkString("Vector(", ", ", ")")
 
   inline def +: [U >: T] (x: U): Vector[U, S[A]] = new +:(x, this)
@@ -46,17 +49,19 @@ object Vector:
 case object VNil extends Vector[Nothing, 0]:
   def apply[At <: Arity](index: At) = throw new IllegalArgumentException(f"Acces on index ${index} of an empty Vector (This means your index was ${index+1} bigger than the size of your Vector)")
   def map[U](f: Nothing => U) = VNil
-  def head() = throw new IllegalArgumentException(f"Empty Vector has no head")
-  def tail() = throw new IllegalArgumentException(f"Empty Vector has no tail")
-  def init() = throw new IllegalArgumentException(f"Empty Vector has no init")
-  def last() = throw new IllegalArgumentException(f"Empty Vector has no last")
+  def head = throw new IllegalArgumentException(f"Empty Vector has no head")
+  def tail = throw new IllegalArgumentException(f"Empty Vector has no tail")
+  def init = throw new IllegalArgumentException(f"Empty Vector has no init")
+  def last = throw new IllegalArgumentException(f"Empty Vector has no last")
   def reverse = VNil
   def fold[U >: Nothing](z: U)(op: (U,U) => U) = z
   def foldLeft[U](z: U)(op: (U, Nothing) => U): U = z
   def mkString(start: String = "", sep: String = "", end: String = ""): String = start ++ end
   def mkString_(sep: String, end: String): String = end
   def toList() = Nil
+  def zip[U](other: Vector[U, 0]) = VNil
   def unzip[T1, T2](implicit asPair: Nothing => (T1, T2)) = (VNil, VNil)
+  def zipWithIndex_(offset: Int) = VNil
   def appended[U](x: U) = x +: VNil
 
 
@@ -69,19 +74,19 @@ final case class +:[+T, A <: Arity](h: T, t: Vector[T,A]) extends Vector[T, S[A]
       t.apply(nIndex)
 
   def map[U](f: T => U) = f(h) +: t.map(f)
-  def head() = h
-  def tail() = t
+  def head = h
+  def tail = t
 
-  def init() =
+  def init =
     def fix[T, A <: Arity](v: Vector[T, S[P[A]]]): Vector[T, A] =  v.asInstanceOf[Vector[T, A]]
     t match
       case VNil => t
-      case _ => val res = h +: t.init(); fix(res)
+      case _ => val res = h +: t.init; fix(res)
   
-  def last() = 
+  def last = 
     t match
       case VNil => h
-      case _ => t.last()
+      case _ => t.last
   def reverse = t.reverse :+ h
 
   def fold[U >: T](z: U)(op: (U,U) => U) =
@@ -97,7 +102,8 @@ final case class +:[+T, A <: Arity](h: T, t: Vector[T,A]) extends Vector[T, S[A]
     val (h1, h2) = asPair(h)
     val (t1, t2) = t.unzip(asPair)
     (h1 +: t1, h2 +: t2)
-
+  def zip[U](other: Vector[U, S[A]]) = (h, other.head) +: t.zip(other.tail)
+  def zipWithIndex_(offset: Int) = (h, offset) +: t.zipWithIndex_(offset + 1)
   def appended[U >: T](x: U): Vector[U, S[S[A]]] = h +: t.appended(x)
   
 end +:
