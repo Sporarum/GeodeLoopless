2aa09c55f4844a9fb6afdd6298a8ee101573c6df
Quentin Bernet <quentin.bernet@bluewin.ch>
2021-02-08 15:48:07 +0100

    Refactor prints to use the s format instead of f

--- src/main/scala/PrimRecFun.scala
+++ src/main/scala/PrimRecFun.scala
@@ -19,42 +19,40 @@ sealed case class UserDefined[A <: Arity](name: String, f: PrimRecFun[A]) extend
   def apply(args: V) = f(args)
   def debug_(args: V) = 
     val res = apply(args) //hides the encased complexity for debugging
-    val n = "\n"
-    (res, f"Function $name on args: $args${n}Returned: $res")
+    (res, s"Function $name on args: $args\nReturned: $res")
+  override def toString = name
 
 
 //f(X) = constant
 sealed case class Const[A <: Arity](val constant: Nat) extends PrimRecFun[A]:
   def apply(args: V) = constant
   def debug_(args: V) =
-    (apply(args), f"Const($constant) on args: $args") //print arity ?
+    (apply(args), s"Const($constant) on args: $args") //print arity ?
 
 
 //f(X) = x_n
 sealed case class Proj[A <: Arity](val n: Arity) extends PrimRecFun[A]:
   def apply(args: V) = args(n)
   def debug_(args: V) =
-    (apply(args), f"Proj($n) on args: $args")
+    (apply(args), s"Proj($n) on args: $args")
 
 
 //f(x) = Succ(x)
 case object Succ extends PrimRecFun[1]:
   def apply(args: V) = SuccNat(args(0))
   def debug_(args: V) =
-    (apply(args), f"Succ on args: $args") 
+    (apply(args), s"Succ on args: $args") 
 
 
 //f(X) = g(f_1(X), f_2(X), ... , f_A1(X)) with X a vector of arity A2
 sealed case class Comp[A1 <: Arity, A2 <: Arity](g: PrimRecFun[A1], fs: Vector[PrimRecFun[A2], A1]) extends PrimRecFun[A2]:
   def apply(args: V) = g(fs.map(_(args)))
   def debug_(args: V) =
-    val n = '\n'
-    val start = f"Comp on args: $args {$n"
     val (resultsFs, strings) = fs.map(_.debug_(args)).unzip
-    val stringsWithNames = strings.toList().zipWithIndex.map{case (s, m) => f"f_$m: {$n$s$n}"} //add result after closing brace ?
-    val stringFs: String = stringsWithNames.mkString(start="", sep="\n", end=f"$n")
+    val stringsWithNames = strings.toList().zipWithIndex.map{case (s, m) => s"f_$m: {\n$s\n}"} //add result after closing brace ?
+    val stringFs: String = stringsWithNames.mkString(sep="\n")
     val (resultG, stringG) = g.debug_(resultsFs)
-    (resultG, start ++ stringFs ++ f"g: {$n$stringG$n}: $resultG" ++ f"$n}: $resultG")
+    (resultG, s"Comp on args: $args {\n$stringFs\ng: {\n$stringG\n}: $resultG\n}: $resultG")
 
 
 // f(X :+ 0) = init(X) with X a vector of arity A1
@@ -69,22 +67,20 @@ sealed case class Rec[A1 <: Arity](base: PrimRecFun[A1], step: PrimRecFun[S[S[A1
         step(init :+ n :+ apply(init :+ n))
 
   def debug_(args: V) =
-    val n = '\n'
     def debug_inner(args: V): (Nat, String) =
       val last = args.last
       val init: Vector[Nat, A1] = args.init
       last match
         case ZeroNat => 
           val (res, s) = base.debug_(init)
-          (res, f"Rec calls init: {$n$s$n}: $res")
+          (res, s"Rec calls init: {\n$s\n}: $res")
         case SuccNat(pred) =>
           val (recRes, recS) = debug_inner(init :+ pred)
           val (stepRes, stepS) = step.debug_(init :+ pred :+ recRes)
           (stepRes, recS ++ "\n" ++ f"Rec calls step: {$n$stepS$n}: $stepRes")
 
     val (res, s) = debug_inner(args)
-    (res, f"Rec on args: $args {$n$s$n}: $res")
-
+    (res, s"Rec on args: $args {\n$s\n}: $res")
 
 extension [A0 <: Arity] (g: PrimRecFun[1])
   def apply(f0: PrimRecFun[A0]) =
